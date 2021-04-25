a63634531dd8fb2280b37fd75bd748fb0ce2f3a3
Adrien Dumont <dev@adum.io>
2021-04-25 20:43:00 +0200

    added installation steps

--- update_extension.sh
+++ update_extension.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-cp reverso_albert.py /usr/share/albert/org.albert.extension.python/modules/reverso_context/__init__.py
+if [ -d /usr/share/albert/org.albert.extension.python/modules/reverso_context ]
+then
+    cp reverso_albert.py /usr/share/albert/org.albert.extension.python/modules/reverso_context/__init__.py
+else
+    pushd .
+    pushd /usr/share/albert/org.albert.extension.python/modules
+    mkdir reverso_context
+    popd
+    cp reverso_albert.py /usr/share/albert/org.albert.extension.python/modules/reverso_context/__init__.py
+    popd
+fi
